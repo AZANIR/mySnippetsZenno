4cc68b01bebe101861bff70583b508fd4e4b61a0
Leonid M <eleoneks@gmail.com>
2020-06-18 12:06:34 +0300

    update snippets from menu zennoposter

---
+++ [1_StandartMenuZenno]/1_Data/8_JSON_Xml/Json_Xml_Parse.cs
@@ -0,0 +1,5 @@
+﻿//парсим Xml текст из переменной
+project.Xml.FromString(project.Variables["XmlText"].Value);
+
+//парсим Json текст из переменной
+project.Json.FromString(project.Variables["JsonText"].Value);
---
+++ [1_StandartMenuZenno]/1_Data/8_JSON_Xml/Json_Xml_ToList.cs
@@ -0,0 +1,36 @@
+﻿//Xml ToList example 1 
+var list_xml = new List<string>();
+for(int i = 0; i < project.Xml.PurchaseOrder.Address.Count; i++)
+{
+	list_xml.Add(project.Xml.PurchaseOrder.Address[i].Name.Value);
+}
+//выводим содержимое списка обьединив через ","
+return string.Join(", ", list_xml);
+
+//Xml ToList example 2 
+var list_xml = new List<string>();
+foreach(dynamic i in project.Xml.PurchaseOrder.Address)
+{
+	list_xml.Add(i.Name.Value);
+}
+//выводим содержимое списка обьединив через ","
+return string.Join(", ", list_xml);
+
+
+//Json ToList example 1 
+var list_json = new List<string>();
+for(int i = 0; i < project.Json.PurchaseOrder.Address.Count; i++)
+{
+	list_json.Add(project.Json.PurchaseOrder.Address[i].Name.Value);
+}
+//выводим содержимое списка обьединив через ","
+return string.Join(", ", list_json);
+
+//Json ToList example 2 
+var list_json = new List<string>();
+foreach(dynamic i in project.Json.PurchaseOrder.Address)
+{
+	list_json.Add(i.Name.Value);
+}
+//выводим содержимое списка обьединив через ","
+return string.Join(", ", list_json);
